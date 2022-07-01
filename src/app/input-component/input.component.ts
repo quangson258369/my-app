ea5c86a9cd3ea3633bbe8be6c97423762382ec60
quangson258369 <sontqhe153230@fpt.edu.vn>
2022-07-01 15:10:34 +0700

    updateV2

---
+++ src/app/input-component/input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'input-component',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent  {
+  deleteTask(){};
+  checkComplete(){};
+  addTask(){};
+}
